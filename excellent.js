2155c5f2b4cbbf85c80731acbafc7ac9bed19bac
TODIREP <tqkrwoqud830@naver.com>
2019-12-10 15:06:26 +0900

    테스트 케이스 테스트

--- excellent.js
+++ excellent.js
@@ -71,11 +71,12 @@ function excellent() {
 	function root(a, b, c) {
 		var ans;
 		ans = a + 1;
+	
+	if (true) {
+		a = 1;
+	}
 		ans = a + 1 + 2;
-		return ans;
+		return ans + root(a,b,c);
 	}
 	a = plus(2 * a + 1,a + 25 * 3);
-	a = a - 100;
-	b = b - 100;
-	a = a + 100;
 }
